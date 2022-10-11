ac7022f71409d26582c17af69b6ce14d5e6f0efe
OrionVi1998 <15966899+OrionVi1998@users.noreply.github.com>
2022-10-11 11:51:53 -0300

    -Update Landing Page Style

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -7,13 +7,61 @@ import {
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Button, Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
 import Cifrar from './components/Cifrar';
 import Descifrar from './components/Descifrar';
 
-const LandingPage = () => {
-  return <div className="LandingPage">Hi</div>;
-};
+const logoGrande =
+  'https://cdn.discordapp.com/attachments/576565786630422548/1015367780259872838/logotipo_pag.png';
+
+function LandingPage() {
+  return (
+    <Container className="childContainer">
+      <Row
+        className="justify-content-md-center"
+        style={{ paddingTop: 50, paddingBottom: 50 }}
+      >
+        <Col md="auto">
+          <img src={logoGrande} alt="" />
+        </Col>
+      </Row>
+      <Row className="justify-content-md-center">
+        <Col
+          md="auto"
+          style={{ color: 'white', height: '100px', fontFamily: 'Fira Sans' }}
+        >
+          <p style={{ fontFamily: 'DM Sans', textAlign: 'center' }}>
+            ¿QUERÉS COMPARTIR TUS CLAVES O GUARDARLAS DE FORMA DISCRETA?
+            <br />
+            HIDDENKEY BRINDA TODO ESTO POR MEDIO DE ESTEGANOGRAFÍA
+          </p>
+        </Col>
+      </Row>
+      <Row className="justify-content-md-center">
+        <Col md="auto">
+          <Button
+            variant="success"
+            // onClick={() => setTab('Cifrado')}
+            className="greenButton"
+            size="lg"
+          >
+            CIFRAR
+          </Button>
+        </Col>
+        <Col md="auto">
+          <Button
+            variant="success"
+            // onClick={() => setTab('Descifrado')}
+            className="greenButton"
+            size="lg"
+          >
+            DESCIFRAR
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
 
 export default function App() {
   return (

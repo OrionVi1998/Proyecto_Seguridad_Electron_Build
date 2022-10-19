6514e470d79f791bac559fa1e6d53a0277088d00
OrionVi1998 <15966899+OrionVi1998@users.noreply.github.com>
2022-10-18 22:11:20 -0300

    ADD utility function decode

--- src/renderer/utilidad/png_hider.ts
+++ src/renderer/utilidad/png_hider.ts
@@ -42,50 +42,34 @@ function encode(buffer: any, message: any) {
   });
 }
 
-function decode() {
-  // return new Promise((resolve, reject) => {
-  //   readeableStream
-  //     .pipe(
-  //       new PNG({
-  //         filterType: 4,
-  //       })
-  //     )
-  //     .on('parsed', function () {
-  //       let message = [];
-  //       let hasMessage = false;
-  //
-  //       for (let y = 0; y < this.height; y++) {
-  //         for (let x = 0; x < this.width; x++) {
-  //           const idx = (this.width * y + x) << 2;
-  //
-  //           // Iterates over every pixel
-  //           const originalPixel = `rgba(${this.data[idx]}, ${
-  //             this.data[idx + 1]
-  //           }, ${this.data[idx + 2]}, ${this.data[idx + 3]})`;
-  //
-  //           // Modifies alpha channel
-  //
-  //           if (this.data[idx + 3] === 254) {
-  //             hasMessage = true;
-  //           }
-  //           if (hasMessage) {
-  //             message.push(255 - this.data[idx + 3]);
-  //           }
-  //
-  //           const modifiedPixel = `rgba(${this.data[idx]}, ${
-  //             this.data[idx + 1]
-  //           }, ${this.data[idx + 2]}, ${this.data[idx + 3]})`;
-  //           // console.log(count,pointer, Math.round(count / (this.height * this.width)), modifiedPixel)
-  //         }
-  //       }
-  //       console.log(message.reverse());
-  //       message.push(0);
-  //       message = message.reverse().join('');
-  //       console.log(binaryToString(message));
-  //
-  //       resolve(this);
-  //     });
-  // });
+function decode(buffer: any) {
+  return new Promise((resolve) => {
+    new PNG({ filterType: 4 }).parse(buffer, function (_error: any, data: any) {
+      let message: any = [];
+      let hasMessage = false;
+
+      for (let y = 0; y < data.height; y += 1) {
+        for (let x = 0; x < data.width; x += 1) {
+          // eslint-disable-next-line no-bitwise
+          const idx = (data.width * y + x) << 2;
+
+          // Modifies alpha channel
+
+          if (data.data[idx + 3] === 254) {
+            hasMessage = true;
+          }
+          if (hasMessage) {
+            message.push(255 - data.data[idx + 3]);
+          }
+        }
+      }
+
+      message.reverse();
+      message.push(0);
+      message = message.reverse().join('');
+      resolve(binaryToString(message));
+    });
+  });
 }
 
 export { encode, decode };
